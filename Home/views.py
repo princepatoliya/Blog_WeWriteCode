f54ce9dcef7113d226445e4eb8ea9b1440c7edab
princepatoliya <princepatoliya852@gmail.com>
2021-06-11 13:23:29 +0530

    change image pixels parts

--- Home/views.py
+++ Home/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import (
+    BlogModel
+    )
+
 from .form import (
     BlogForm
     )
@@ -11,9 +15,30 @@ def home_screen_view(request):
 
 
 def add_blog(request):
+    try:
+        if request.method == "POST":
+            print("i'm working")
+            form = BlogForm(request.POST)
+            print("from, ", form)
+            image = request.FILES['uploadedimage']
+            title = request.post.get('title')
+            user = request.user
+
+            if form.is_valid():
+                content = form.cleaned_data['content']
+
+
+            blog_obj = BlogModel.objects.create(user=user, title=title, content=content, image=image)
+            print("Blog object", blog_obj)
+
+    except Exception as e:
+        print(e) 
+
+
     data = {
     'form' : BlogForm
     }
+
     return render(request, 'home/add_blog.html', data)
 
 
